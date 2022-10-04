7d8ad03b5517ca99a636819dc1c7e997666863a9
Gustavo Oliva <gustavociviloliva@gmail.com>
2022-10-04 19:30:14 -0300

    starting project

---
+++ src/main/kotlin/com/oliva/pontointeligente/enums/PerfilEnum.kt
@@ -0,0 +1,6 @@
+package com.oliva.pontointeligente.enums
+
+enum class PerfilEnum {
+    ROLE_ADMIN,
+    ROLE_USUARIO
+}
---
+++ src/main/kotlin/com/oliva/pontointeligente/repositories/EmpresaRepository.kt
@@ -0,0 +1,8 @@
+package com.oliva.pontointeligente.repositories
+
+import com.oliva.pontointeligente.documents.Empresa
+import org.springframework.data.mongodb.repository.MongoRepository
+
+interface EmpresaRepository : MongoRepository<Empresa, String> {
+    fun findByCnpj(cnpj: String): Empresa?
+}
